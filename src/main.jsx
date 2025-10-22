b0b24d8a4dd1975b35b04f8461ba59d38bf3ad22
Ibukunoluwa Akiode <ibukunakiode3@gmail.com>
2025-10-22 22:21:21 +0100

    Update Fonts

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import "@fontsource/outfit";
-import "@fontsource/roboto"; 
+import "@fontsource/outfit/400.css";
+import "@fontsource/outfit/700.css";
+import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 
